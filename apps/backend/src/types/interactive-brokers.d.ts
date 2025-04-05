059cb9ee3bafdf0804fd01dcf1e8af44f11fbc47
Jonny Furness <jlsfurness@gmail.com>
2025-04-05 09:26:54 -0400

    feat: 🎸 Added xml parsing for IB

---
+++ apps/backend/src/types/interactive-brokers.d.ts
@@ -0,0 +1,55 @@
+import { State } from 'node_modules/@payloadcms/ui/dist/elements/WhereBuilder/types'
+
+type ReferenceCodeSuccessReponse = {
+  Status: 'Success'
+  ReferenceCode: string
+}
+
+type ReferenceCodeErrorResponse = {
+  Status: 'Fail'
+  ErrorCode: number
+  ErrorMessage: string
+}
+
+export type ReferenceCodeResponse = {
+  FlexStatementResponse: ReferenceCodeSuccessReponse | ReferenceCodeErrorResponse
+}
+
+export interface FlexQueryReport {
+  FlexQueryResponse: {
+    FlexStatements: {
+      FlexStatement: {
+        '@_toDate': string
+        '@_accountId': string
+        '@_fromDate': string
+        '@_toDate': string
+        '@_period': string
+        '@_whenGenerated': string
+        EquitySummaryInBase: {
+          EquitySummaryByReportDateInBase: {
+            '@_total': string
+          }
+        }
+        CashReport: {
+          CashReportCurrency: {
+            '@_depositWithdrawals': string
+            '@_depositwithdrawalssec': string
+            '@_depositwithdrawalscom': string
+            '@_depositwithdrawalsmtd': string
+            '@_depositwithdrawalsytd': string
+            '@_accounttransfers': string
+            '@_accounttransferssec': string
+            '@_accounttransferscom': string
+            '@_accounttransfersmtd': string
+            '@_accounttransfersytd': string
+            '@_internaltransfers': string
+            '@_internaltransferssec': string
+            '@_internaltransferscom': string
+            '@_internaltransfersmtd': string
+            '@_internaltransfersytd': string
+          }
+        }
+      }
+    }
+  }
+}
